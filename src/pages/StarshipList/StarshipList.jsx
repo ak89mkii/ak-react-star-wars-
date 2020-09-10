bf53ebd2763b0a5d201d23d14e1a317a4ce5bd38
A.I. Kasem <ak89mkii@gmail.com>
2020-09-10 18:22:37 -0400

    test

---
+++ src/pages/StarshipList/StarshipList.jsx
@@ -0,0 +1,38 @@
+import React, {Component} from 'react'
+import { getStarshipList } from '../../services/api-calls'
+import { Link } from "react-router-dom"
+
+class StarshipList extends Component {
+    state = {
+        starshipList: []
+    }
+
+
+async componentDidMount() {
+    const starshipList = await getStarshipList()
+    console.log(starshipList)
+    this.setState({ starshipList: starshipList.results })
+}
+
+render() {
+    return (
+        <>
+            <h1>Starship List</h1>
+            {this.state.starshipList.map((starship) =>
+                <div key={starship.url}>
+                    <Link
+                    to={{
+                        pathname: '/starship',
+                        state: {starship}
+                    }}
+                >
+                    {starship.name}
+                    </Link><br></br>
+                </div>
+            )}
+        </>
+    )
+}
+}
+
+export default StarshipList
